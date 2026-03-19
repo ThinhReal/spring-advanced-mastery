cf359416d5fcfa3a1a5b08d85f0d48883018ca6d
Thinh Nguyen <thinhlinhtinh2006@gmail.com>
2026-03-19 10:13:27 +0700

    chore minor changes

--- src/main/java/com/thinhreal/spring_advanced_mastery/service/UserService.java
+++ src/main/java/com/thinhreal/spring_advanced_mastery/service/UserService.java
@@ -7,18 +7,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
-
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-
     public UserDTO getUserById(Long id) {
         // 1. Fetch from "DB" (using a dummy user if not found)
         UserEntity entity = userRepository.findById(id)
                 .orElse(new UserEntity(1L, "Thinh Real", "thinh@rmit.edu.vn", "secret_pass_123"));
-
         // 2. Map Entity -> DTO (The most important part!)
         UserDTO dto = new UserDTO();
         dto.setUsername(entity.getUsername());
